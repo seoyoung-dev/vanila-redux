290639518eea31d74b04db58d748e02e002720e3
seoyoung-dev <seoyoung.dev@gmail.com>
2022-01-12 03:33:24 +0900

    ✨ pure redux: counter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// state = 0 을 통해 initializing(default 값을 정의함)
-const countModifier = (count = 0) => {
-    console.log(state);
-    return count;
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
+    }
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => {
+    countStore.dispatch({ type: ADD });
+});
+
+minus.addEventListener("click", () => {
+    countStore.dispatch({ type: MINUS });
+});
