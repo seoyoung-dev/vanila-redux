e09fbbb8ab39028a6eb2a9d653bb45f0e8f454eb
seoyoung-dev <seoyoung.dev@gmail.com>
2022-01-20 05:54:24 +0900

    ⚡️ createAction

--- src/store.js
+++ src/store.js
@@ -1,28 +1,15 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addTodo = (text) => {
-    return {
-        type: ADD,
-        text,
-    };
-};
-
-const deleteTodo = (id) => {
-    return {
-        type: DELETE,
-        id: parseInt(id),
-    };
-};
+const addTodo = createAction("ADD");
+const deleteTodo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
     switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter((toDo) => toDo.id !== action.id);
+        case addTodo.type:
+            return [{ text: action.payload, id: Date.now() }, ...state];
+        case deleteTodo.type:
+            return state.filter((toDo) => toDo.id !== action.payload);
         default:
             return state;
     }
